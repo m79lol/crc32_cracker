ce28347e2f389e36bcfc8c731265a69bd875d3fb
m79lol <mikhail@tulkin.name>
2013-09-04 17:06:16 +0600

    Added multi-threading

---
+++ example.bat
@@ -0,0 +1,10 @@
+crc32_cracker.exe
+pause
+crc32_cracker.exe 023113 text
+pause
+crc32_cracker.exe 0123456789 text
+pause
+crc32_cracker.exe -t 0x12345678 text
+pause
+crc32_cracker.exe -t 2 0x12345678 text
+pause
